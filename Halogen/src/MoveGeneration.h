5227a32487007c54adce8a96b66a116e06f4e6ca
Aryan1508 <68417405+Aryan1508@users.noreply.github.com>
2021-03-15 14:46:36 +0530

    .

--- Halogen/src/MoveGeneration.h
+++ Halogen/src/MoveGeneration.h
@@ -8,6 +8,7 @@
 // the ExtendedMove vector from the start.
 struct ExtendedMove
 {
+	ExtendedMove() = default;
 	ExtendedMove(const Move _move, const int _score = 0, const short int _SEE = 0) : move(_move), score(_score), SEE(_SEE) {}
 	ExtendedMove(Square from, Square to, MoveFlag flag) 
 		: move(CreateMove(from, to, flag)), score(0), SEE(0) {}
@@ -16,8 +17,8 @@ struct ExtendedMove
 	bool operator>(const ExtendedMove& rhs) const { return score > rhs.score; };
 
 	Move move = Move::invalid;
-	int16_t score;
-	int16_t SEE;
+	int16_t score = 0;
+	int16_t SEE = 0;
 };
 
 void LegalMoves(Position& position, std::vector<ExtendedMove>& moves);
